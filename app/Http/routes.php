e015a1cfea82eea51521d869da41cb0a49efa011
a6Brgeuka <naranov93@mail.ru>
2016-02-01 04:55:11 +0300

    Basic MVC WorkFlow

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('about', 'PagesController@about');
 
 Route::get('contact', 'PagesController@contact');
 
+Route::get('articles', 'ArticlesController@index');
+
+Route::get('articles/{id}', 'ArticlesController@show');
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
